0f035f5221cc570298158c6681b2069e9b950a89
labiss96 <cxz9080@gmail.com>
2020-08-22 17:55:39 +0900

    implement Chat list and detail frame. connect chatting flow.

---
+++ src/components/content/Chat.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+
+import { List } from "react-native-paper";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { AuthContext } from "../../context";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingLeft: wp("10%"),
+    paddingRight: wp("10%"),
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+const ChatView = ({ name, description, id, navigation }) => (
+  <List.Item
+    title={name}
+    description={description}
+    left={(props) => <List.Icon {...props} icon="account" />}
+    onPress={() => navigation.push("ChatDetail")}
+  />
+);
+
+export const Chat = ({ navigation }) => {
+  const { signOut } = React.useContext(AuthContext);
+  const [chatData, setChatData] = useState([]);
+  useEffect(() => {
+    const dummy = [
+      {
+        name: "이정현",
+        last_message: "테스트 메시지다..",
+        chat_id: "1",
+      },
+      {
+        name: "박종민",
+        last_message: "테스트 메시지야 시발",
+        chat_id: "2",
+      },
+    ];
+
+    setChatData(dummy);
+  }, []);
+  return (
+    <>
+      <View>
+        {chatData.map((elem, idx) => (
+          <ChatView
+            key={idx}
+            name={elem.name}
+            description={elem.last_message}
+            id={elem.chat_id}
+            navigation={navigation}
+          />
+        ))}
+      </View>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useContext, useReducer } from "react";
+import { StyleSheet, Button } from "react-native";
+import { IconButton, Colors } from "react-native-paper";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
@@ -16,9 +19,40 @@ import { Home } from "./components/content/Home";
 import { Survivor } from "./components/content/Survivor";
 import { MyPage } from "./components/content/Mypage";
 
-import { Chat } from "./components/Chat";
+import { Chat } from "./components/content/Chat";
+import { ChatDetail } from "./components/content/ChatDetail";
 
 import Progress from "./components/Progress";
+
+const ChatStack = createStackNavigator();
+const ChatStackScreen = () => (
+  <ChatStack.Navigator>
+    <ChatStack.Screen
+      name="ChatList"
+      component={Chat}
+      options={{
+        title: "Chat List",
+      }}
+    />
+    <ChatStack.Screen
+      name="ChatDetail"
+      component={ChatDetail}
+      options={{
+        title: "User",
+        // title: (props) => <LogoTitle {...props} />,
+        headerRight: () => (
+          <IconButton
+            icon="exit-to-app"
+            color={Colors.red500}
+            size={20}
+            animated={true}
+            onPress={() => alert("exit chatting room!")}
+          />
+        ),
+      }}
+    />
+  </ChatStack.Navigator>
+);
 
 const Tabs = createMaterialBottomTabNavigator();
 const TabsScreen = () => (
@@ -27,13 +61,14 @@ const TabsScreen = () => (
       name="Survivor"
       component={Survivor}
       options={{
+        title: "Survivor List",
         tabBarLabel: "Survivors",
         tabBarIcon: ({ color }) => <Icon name="hail" color={color} size={26} />,
       }}
     />
     <Tabs.Screen
       name="Chat"
-      component={Chat}
+      component={ChatStackScreen}
       options={{
         tabBarLabel: "Chat",
         tabBarIcon: ({ color }) => <Icon name="chat" color={color} size={26} />,
@@ -43,6 +78,7 @@ const TabsScreen = () => (
       name="MyPage"
       component={MyPage}
       options={{
+        title: "My Page",
         tabBarLabel: "My Page",
         tabBarIcon: ({ color }) => (
           <Icon name="account-circle" color={color} size={26} />
@@ -68,7 +104,7 @@ const AuthStackScreen = () => (
     <AuthStack.Screen
       name="Auth"
       component={Auth}
-      options={{ title: "E-mail Auth" }}
+      options={{ title: "E-mail 인증" }}
     />
   </AuthStack.Navigator>
 );
@@ -182,6 +218,7 @@ export default () => {
 
   useEffect(() => {
     console.log("run useEffect : Index");
+    setReportFlag(false);
     setTimeout(async () => {
       let userToken = null;
       try {
