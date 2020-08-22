b71650dbdbda289e2b22046f933e3b2a0820ddbe
labiss96 <cxz9080@gmail.com>
2020-08-22 15:54:29 +0900

    render survivor list.

--- src/components/content/Survivor.js
+++ src/components/content/Survivor.js
@@ -23,6 +23,34 @@ const ScreenContainer = ({ children }) => (
   <View style={styles.container}>{children}</View>
 );
 
+const ListView = ({ elem, index }) => {
+  let component = "";
+  switch (index) {
+    case 1:
+      component = (
+        <View>
+          <Text>최고의 생존자 '{elem.name}'님</Text>
+          <Text>생존 시각 : {elem.pub_date}</Text>
+          <Text>---------------------------------------------------------</Text>
+        </View>
+      );
+      break;
+    default:
+      component = (
+        <View>
+          <Text>
+            {index}위 '{elem.name}'님
+          </Text>
+          <Text>생존 시각 : {elem.pub_date}</Text>
+          <Text>
+            ----------------------------------------------------------------
+          </Text>
+        </View>
+      );
+  }
+  return component;
+};
+
 export const Survivor = ({ navigation }) => {
   const { signOut } = React.useContext(AuthContext);
   const [survivors, setSurvivors] = useState([]);
@@ -41,8 +69,12 @@ export const Survivor = ({ navigation }) => {
   return (
     <ScreenContainer>
       <View>
-        <Text>Survivor Screen!</Text>
+        <Text>생존자 리스트</Text>
+        <Text>=======================================</Text>
       </View>
+      {survivors.map((elem, idx) => (
+        <ListView key={idx} elem={elem} index={idx + 1} />
+      ))}
       <Button title="Sign Out" onPress={() => signOut()} />
     </ScreenContainer>
   );
